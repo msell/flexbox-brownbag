0c4e934313a5a159980f891f2f959f2e949aab89
Matt Sell <mwsell@gmail.com>
2016-07-24 11:08:08 -0500

    autoprefixer build step

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    open = require('opn');
+    open = require('opn'),
+    autoprefixer = require('gulp-autoprefixer');;
 
 gulp.task('connect', function () {
     connect.server({
-        root: 'src',
+        root: 'build',
         livereload: true
     });
 });
 
 gulp.task('html', function () {
-    gulp.src('./src/index.html')        
+    gulp.src('src/index.html')
+    .pipe(gulp.dest('build'));
+
+    gulp.src('./build/index.html')
         .pipe(connect.reload());
+});
+
+gulp.task('styles', function(){
+    gulp.src('src/style.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build'))
 });
 
 gulp.task('watch', function () {
@@ -19,4 +29,4 @@ gulp.task('watch', function () {
     open('http://localhost:8080');
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['html', 'styles', 'connect', 'watch']);
--- readme.md
+++ readme.md
@@ -312,6 +312,12 @@ add flex-grow: 2; to box2 it will give box2 twice the extra space as box 1
 - now add flex-basis:100px; to box4.  crush the browser up and down vertically.
 
 13) Cross Browser Flexbox Support + Autoprefixer
+[https://css-tricks.com/old-flexbox-and-new-flexbox](old and new flexbox)
+- flexbox spec has changed over time and not all vendors have caught up or you might be dealing w/ older browsers
+- http://autoprefixer.github.io
+
+- the idea here is that you write your code in the latest greatest flex box spec then run it through an auto-prefixer build step
+ 
 14) Pure Flexbox navigation code along
 15) Mobile content reordering with Flexbox
 16) Nesting Flexbox for vertical and horizontal centering
